0e5d078a63d1be9bc640aeae0508fb52043df2eb
Todd Herron <todd.c.herron@gmail.com>
2020-10-08 16:26:43 -0600

    Re-commit of previous changes

---
+++ android/app/src/main/kotlin/app/pennies_from_heaven/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.pennies_from_heaven
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,26 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    
+    let controller = self.window.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel.init(name: "flavor", binaryMessenger: controller.binaryMessenger)
+    
+    channel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+    // Note: this method is invoked on the UI thread
+    // Handle battery messages.
+    let flavor = Bundle.main.infoDictionary?["Flavor"]
+    result(flavor)
+    })
+    
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
