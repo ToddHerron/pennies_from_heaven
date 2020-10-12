68e901393b11ee9f162a27501993f3d9b003174b
Todd Herron <todd.c.herron@gmail.com>
2020-10-12 00:46:25 -0600

    Implement Multi-provider & Avatar Photo selection

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,9 +1,17 @@
 import 'dart:async';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:pennies_from_heaven/app/auth_widget.dart';
 import 'package:pennies_from_heaven/app/home/about_page.dart';
 import 'package:pennies_from_heaven/common_widgets/avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:pennies_from_heaven/models/avatar_reference.dart';
 import 'package:pennies_from_heaven/services/firebase_auth_service.dart';
+import 'package:pennies_from_heaven/services/firebase_storage_service.dart';
+import 'package:pennies_from_heaven/services/firestore_path.dart';
+import 'package:pennies_from_heaven/services/firestore_service.dart';
+import 'package:pennies_from_heaven/services/image_picker_service.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -28,9 +36,24 @@ class HomePage extends StatelessWidget {
   Future<void> _chooseAvatar(BuildContext context) async {
     try {
       // 1. Get image from picker
-      // 2. Upload to storage
-      // 3. Save url to Firestore
-      // 4. (optional) delete local file as no longer needed
+      final imagePicker = Provider.of<ImagePickerService>(context);
+      final file = await imagePicker.pickImage(source: ImageSource.gallery);
+
+      // 2. Upload the image to remote Storage / get url
+      if (file != null) {
+        final storage = Provider.of<FirebaseStorageService>(context);
+        final user = Provider.of<User>(context, listen: false);
+        final downloadUrl =
+            await storage.uploadAvatar(uid: user.uid, file: file);
+
+        // 3. Save url to remote Firestore
+        final database = Provider.of<FirestoreService>(context);
+        await database.setAvatarReference(
+            uid: user.uid, avatarReference: AvatarReference(downloadUrl));
+
+        // 4. Delete the local file
+        await file.delete();
+      }
     } catch (e) {
       print(e);
     }
@@ -67,17 +90,29 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      body: Center(
+          child: CircularProgressIndicator(
+        strokeWidth: 6.0,
+        backgroundColor: Colors.limeAccent[700],
+      )),
     );
   }
 
   Widget _buildUserInfo({BuildContext context}) {
-    // TODO: Download and show avatar from Firebase storage
-    return Avatar(
-      photoUrl: null,
-      radius: 50,
-      borderColor: Colors.black54,
-      borderWidth: 2.0,
-      onPressed: () => _chooseAvatar(context),
-    );
+    final database = Provider.of<FirestoreService>(context);
+    final user = Provider.of<User>(context, listen: false);
+
+    return StreamBuilder<AvatarReference>(
+        stream: database.avatarReferenceStream(uid: user.uid),
+        builder: (context, snapshot) {
+          final avatarReference = snapshot.data;
+          return Avatar(
+            photoUrl: avatarReference?.downloadUrl,
+            radius: 50,
+            borderColor: Colors.black54,
+            borderWidth: 2.0,
+            onPressed: () => _chooseAvatar(context),
+          );
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:pennies_from_heaven/app/auth_widget.dart';
 
 import 'package:flutter/material.dart';
 import 'package:pennies_from_heaven/services/firebase_auth_service.dart';
+import 'package:pennies_from_heaven/services/firebase_storage_service.dart';
+import 'package:pennies_from_heaven/services/firestore_service.dart';
+import 'package:pennies_from_heaven/services/image_picker_service.dart';
 import 'package:provider/provider.dart';
 
 import 'common/initializingFunctions.dart';
@@ -15,8 +18,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Provider<FirebaseAuthService>(
-      create: (_) => FirebaseAuthService(),
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthService>(create: (_) => FirebaseAuthService()),
+        Provider<ImagePickerService>(create: (_) => ImagePickerService()),
+        Provider<FirebaseStorageService>(
+            create: (_) => FirebaseStorageService()),
+        Provider<FirestoreService>(create: (_) => FirestoreService()),
+      ],
       child: MaterialApp(
         theme: ThemeData(primarySwatch: Colors.indigo),
         home: AuthWidget(),
